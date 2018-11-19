712e34417e8c47283bc4419c9817c3f7ab96bcd2
jinelei <jinelei@163.com>
2018-11-19 16:51:59 +0800

    添加测试

---
+++ lib/tools/debug.dart
@@ -0,0 +1,17 @@
+class Debug {
+  static log(String tag, String text) {
+    print('[$tag] $text');
+  }
+
+  static info(String tag, String text) {
+    print('[$tag] $text');
+  }
+
+  static success(String tag, String text) {
+    print('[$tag] $text');
+  }
+
+  static error(String tag, String text) {
+    print('[$tag] $text');
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -16,15 +16,12 @@ void main() {
     await tester.pumpWidget(MyApp());
 
     // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.byKey(new Key('search_icon_btn')), findsOneWidget);
+    expect(find.byKey(new Key('search_icon_btn0')), findsNothing);
 
     // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    await tester.tap(find.byKey(new Key('search_icon_btn')));
     await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
   });
 }
