58d6c9f1cdee7fdac64d00d109b5f2c5484c6b74
jinelei <jinelei@163.com>
2018-11-19 16:51:47 +0800

    添加全局变量

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,32 @@
 // Step 4: Create an infinite scrolling lazily loaded list
 
 import 'package:flutter/material.dart';
-import './pages/home-page.dart';
-import './pages/splash-page.dart';
-import './pages/other-page.dart';
-import './pages/second-page.dart';
-import './pages/random-words.dart';
+import 'package:flutter/rendering.dart';
+import './pages/home.dart';
+import './pages/splash.dart';
+import 'package:jinelei/component/book.dart';
+import 'package:jinelei/component/moive.dart';
+import 'package:jinelei/component/random-words.dart';
 
-void main() => runApp(MyApp());
+var globalSetting;
+
+void main() {
+  debugPaintSizeEnabled = !true;
+  globalSetting = {
+    'test': true
+  };
+  return runApp(MyApp());
+}
 
 MaterialApp MyApp() {
   final Map<String, WidgetBuilder> _routes = <String, WidgetBuilder>{
     Navigator.defaultRouteName: (context) => new HomePage(),
     '/splash': (context) => new SplashPage(),
-    '/other': (context) => new OtherPage('other'),
-    '/second': (context) => new SecondPage('second'),
     '/random': (context) => new RandomWordsPage(),
   };
 
   return new MaterialApp(
     routes: _routes,
-    initialRoute: '/splash',
+    initialRoute: '/',
   );
 }
